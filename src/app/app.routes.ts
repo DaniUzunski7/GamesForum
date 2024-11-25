0f0c04539e2442e7ad42ba2f56ac95e521276bf8
DANIU-PC\danie <daniel.pleven@abv.bg>
2024-11-25 23:23:44 +0200

    Added Top Games page and updated styling for background image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { TopGamesComponent } from './top-games/top-games.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomePageComponent},
 
     {path: 'login', component: LoginPageComponent},
+
+    {path: 'register', component: RegisterPageComponent},
+
+    {path: 'top-games', component: TopGamesComponent}
 ];
---
+++ src/app/types/game-type.ts
@@ -0,0 +1,9 @@
+export interface Game {
+    title: string,
+    cover: string,
+    description: string,
+    date: string,
+    platforms: string,
+    publisher: string,
+    place: number
+}
