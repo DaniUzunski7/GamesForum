4226732f7a540c7891f54b0a0e498e38e776bfe2
DANIU-PC\danie <daniel.pleven@abv.bg>
2024-11-26 21:55:40 +0200

    made small changes

--- src/app/top-games/top-games.component.ts
+++ src/app/top-games/top-games.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Game } from '../types/game-type';
 import { retry } from 'rxjs';
+import { Game } from '../types/game-type';
+import { games } from '../data/top-10-games';
 
 @Component({
   selector: 'app-top-games',
@@ -11,106 +12,8 @@ import { retry } from 'rxjs';
   styleUrl: './top-games.component.css'
 })
 export class TopGamesComponent {
-  games: Game[] = [
-    {
-      title: 'Black Myth: Wukong',
-      cover: '/black-myth-wukong.jpg',
-      description: 'Black Myth: Wukong is an action RPG inspired by the classic Chinese novel Journey to the West. Players take on the role of the Monkey King, Wukong, to battle mythical creatures in a visually stunning, dark-fantasy world. The game combines fast-paced combat, deep storytelling, and breathtaking visuals , delivering an immersive experience rooted in Chinese folklore.', 
-      date: '20-08-2024', 
-      platforms: 'Playstation 5', 
-      publisher: 'Game Science',
-      place: 1
-    },
-
-    {title: 'Elden Ring: Shadow Of The Erdtree',
-      cover: '/EldenRing.jpg', 
-      description: 'Elden Ring: Shadow of the Erdtree is an expansion to the critically acclaimed Elden Ring, diving deeper into the lore of the Lands Between. Players will uncover new mysteries surrounding the Erdtree while exploring a vast, hauntingly beautiful world filled with new enemies, treacherous landscapes, and challenging bosses.', 
-      date: '20-06-2024', 
-      platforms: 'PlayStation 5, PlayStation 4, Xbox One, Xbox Series X and Series S, Microsoft Windows', 
-      publisher: 'FromSoftware Inc.',
-      place: 2
-    },
-
-    {
-      title: 'Final Fantasy VII Rebirth',
-      cover: '/ffvii_rebirth.jpg', 
-      description: 'Final Fantasy VII Rebirth is the highly anticipated sequel in the Final Fantasy VII Remake series, continuing the reimagined journey of Cloud Strife and his allies. Set in an expansive world beyond Midgar, the game explores new environments, deepens character relationships, and introduces thrilling twists to the original story.', 
-      date: '29-02-2024', 
-      platforms: 'Playstation 5', 
-      publisher: 'Square Enix Holdings',
-      place: 3
-    },
-
-    {
-      title: 'Stellar Blade',
-      cover: '/hq720.jpg', 
-      description: 'Stellar Blade is a visually stunning action RPG that follows Eve, a warrior on a mission to save Earth from an alien invasion. Set in a dystopian, sci-fi world, the game features fast-paced, skill-based combat, intricate enemy designs, and a gripping story about survival and humanity.', 
-      date: '26-04-2024', 
-      platforms: 'Playstation 5', 
-      publisher: 'Sony Interactive Entertainment',
-      place: 4
-    },
-
-    {
-      title: 'Warhammer 40K: Space Marine 2',
-      cover: '/warhamer.jpg', 
-      description: 'Warhammer 40K: Space Marine 2 is a thrilling third-person action game that puts players in the boots of a mighty Ultramarine. As Captain Titus, you will battle relentless waves of Tyranids in epic, visceral combat across grim, war-torn worlds.', 
-      date: '09-09-2024', 
-      platforms: 'PlayStation 5, Xbox Series X and Series S, NVIDIA GeForce NOW, Microsoft Windows', 
-      publisher: 'Pullup Entertainment',
-      place: 5
-    },
-
-    {
-      title: 'Helldivers 2',
-      cover: '/helldivers.jpg', 
-      description: 'Helldivers 2 is a cooperative third-person shooter where players take on the role of elite soldiers fighting to protect Super Earth. Team up with friends to tackle challenging missions, battle alien hordes, and use teamwork to survive in hostile environments. ', 
-      date: '08-02-2024', 
-      platforms: 'PlayStation 5, Microsoft Windows', 
-      publisher: 'PlayStation Studios, SONY',
-      place: 6
-    },
-    
-    {
-      title: 'Astro Bot',
-      cover: '/astro.jpg', 
-      description: 'Astro Bot is a charming platformer featuring Astro, a lovable robot exploring vibrant, imaginative worlds. With clever level design, immersive gameplay, and innovative use of VR, the game offers a delightful experience packed with fun challenges, hidden secrets, and heartwarming moments.',
-      date: '06-09-2024', 
-      platforms: 'PlayStation 5, Android', 
-      publisher: 'Sony Interactive Entertainment',
-      place: 7
-    },
-
-    {
-      title: 'Silent Hill 2',
-      cover: '/silenthill.jpg', 
-      description: 'Silent Hill 2 is a psychological horror masterpiece that follows James Sunderland as he journeys to the eerie town of Silent Hill after receiving a mysterious letter from his deceased wife.', 
-      date: '07-10-2024', 
-      platforms: 'PlayStation 5, Microsoft Windows', 
-      publisher: 'Konami',
-      place: 8
-    },
-
-    {
-      title: 'Palworld',
-      cover: '/palworld.jpg', 
-      description: 'Palworld is an open-world survival game where players explore a vibrant world filled with mysterious creatures called "Pals." You can befriend, battle, and even farm with these creatures while building a life in a stunning yet dangerous environment.',
-      date: '19-01-2024', 
-      platforms: 'PlayStation 5, Xbox One, Xbox Series X and Series S, NVIDIA GeForce NOW, Microsoft Windows, Mac OS', 
-      publisher: 'Pocket Pair, Inc.',
-      place: 9
-    },
-
-    {
-      title: 'Call of Duty: Black Ops 6',
-      cover: '/callofduty.jpg', 
-      description: 'Call of Duty: Black Ops 6 delivers an intense first-person shooter experience, continuing the iconic Black Ops saga with a gripping story, fast-paced multiplayer, and innovative gameplay features. Black Ops 6 raises the bar for action and tactical warfare in the series.', 
-      date: '25-10-2024', 
-      platforms: 'PlayStation 5, Xbox Series X and Series S, PlayStation 4, Xbox One, Xbox Cloud Gaming, Microsoft Windows', 
-      publisher: 'Activision',
-      place: 10
-    },
-  ]
+  
+  topGames: Game[] = games;
 
   currentPage: number = 1;
   gamesPerPage: number = 4;
@@ -125,11 +28,11 @@ export class TopGamesComponent {
     return this.topList;
   }
 
-  totalPages: number = Math.ceil(this.games.length / this.gamesPerPage);
+  totalPages: number = Math.ceil(this.topGames.length / this.gamesPerPage);
 
   getOnePageGames(){
     const startIndex = (this.currentPage - 1) * this.gamesPerPage;
-    return this.games.slice(startIndex, startIndex + this.gamesPerPage) 
+    return this.topGames.slice(startIndex, startIndex + this.gamesPerPage) 
   }
 
   goToNextPage(){
