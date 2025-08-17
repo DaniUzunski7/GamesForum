212a4ada825f2dd77acbcbeb781dff120673b7e3
Daniel Uzunski <dani.pleven@icloud.com>
2025-08-17 03:09:24 +0300

    Rebuild register auth service
    delete firebaseUser service

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,43 +1,87 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile, user, UserCredential} from '@angular/fire/auth';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  user,
+  UserCredential,
+} from '@angular/fire/auth';
 import { from, Observable } from 'rxjs';
 import { userForAuth } from '../types/user';
 import { userInterface } from '../types/userInterface';
-
-
+import { collection, doc, Firestore, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  firebaseAuth = inject(Auth);
+    USER_KEY = 'user';
 
-  constructor(private auth: Auth) { }
-  
+  private firebaseAuth = inject(Auth);
+  private firestore = inject(Firestore);
+  private usersCollection = collection(this.firestore, 'users');
+
+  constructor(private auth: Auth) {}
+
   user$ = user(this.firebaseAuth);
   currUser = signal<userInterface | null | undefined>(undefined);
 
   getUser() {
     return this.auth.currentUser;
   }
-  
-  register(userName: string, email: string, password: string): Observable<void> {
-    const promise = createUserWithEmailAndPassword(this.auth, email, password)
-                    .then(response => updateProfile(response.user, {displayName: userName}))
+
+  register(user: userForAuth): Observable<void> {
+    
+    const data = createUserWithEmailAndPassword(
+      this.auth,
+      user.email,
+      user.password
+    )
+      .then((response) =>
+        updateProfile(response.user, { displayName: user.userName })
+      )
+      .then(() => {
+        console.log(this.auth.currentUser);
+        
+        const userRef = doc(this.usersCollection, this.auth.currentUser!.uid);
+        setDoc(userRef, {
+          userName: user.userName,
+          name: user.name,
+          id: this.auth.currentUser!.uid,
+          email: this.auth.currentUser!.email,
+          //! Hash pasword
+          password: user.password,
+          phone: user.phone,
+          createdAt: new Date(),
+        });
+      });
+
+        const userData = {
+        id: this.auth.currentUser!.uid,
+        email: user.email,
+        userName: user.userName,
+        name: user.name,
+        phone: user.phone
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(userData));
+
+    return from(data);
+  }
+
+  login(email: string, password: string) {
+    const promise = signInWithEmailAndPassword(this.auth, email, password).then(
+      () => {}
+    );
 
     return from(promise);
   }
 
-  login(email: string, password: string){
-    const promise = signInWithEmailAndPassword(this.auth, email, password)
-                    .then(() => {});
-
-    return from(promise)
-  }
-
   logout(): Observable<void> {
     const promise = signOut(this.auth);
-
+    localStorage.removeItem('user');
     return from(promise);
   }
 }
--- src/app/user/register-page/register-page.component.ts
+++ src/app/user/register-page/register-page.component.ts
@@ -2,12 +2,10 @@ import { Component, ViewChild, ViewChildren } from '@angular/core';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { EmailValidatorDirective } from '../../directives/email.directive';
-import { UserService } from '../user.service';
 import { DOMAINS } from '../../constants/domains';
 import { AuthService } from '../auth.service';
 import { userForAuth } from '../../types/user';
 import { ToastrService } from 'ngx-toastr';
-import { FirebaseUserService } from '../../data/firebaseUser.service';
 
 @Component({
   selector: 'app-register-page',
@@ -24,7 +22,6 @@ export class RegisterPageComponent {
   errorMessage: string | null = null;
 
   constructor(
-    private userService: FirebaseUserService,
     private router: Router,
     private authService: AuthService,
     private toastr: ToastrService
@@ -40,15 +37,15 @@ export class RegisterPageComponent {
       return;
     }
 
-    this.userService.register(form)
-      .then(() => {
+    try {
+      this.authService.register(form).subscribe(() => {
         this.toastr.success('Checkpoint reached!', 'Successful Registration');
         this.router.navigate(['/home']);
-      })
-      .catch((error) => {
-        this.errorMessage = error.message;
-        this.toastr.error(`Registration failed: ${error.message}`, 'Error');
-        return;
       });
+    } catch (error: any) {
+      this.errorMessage = error.message;
+      this.toastr.error(`Registration failed: ${error.message}`, 'Error');
+      return;
+    }
   }
 }
